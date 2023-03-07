c39ca6b835fca7f141b544a926e20f6b39e4871e
Shaeen09 <shaheenshaikh09@gmail.com>
2023-03-07 21:03:17 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroHomeComponent } from './hero-home/hero-home.component';
+import { HeroPageComponent } from './hero-page/hero-page.component';
+import { FooterHeroComponent } from './footer-hero/footer-hero.component';
+import { AboutComponent } from './about/about.component';
+import { HeroAboutComponent } from './about-page/hero-about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeroHomeComponent,
+    HeroPageComponent,
+    FooterHeroComponent,
+    AboutComponent,
+    HeroAboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/footer-hero/footer-hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-hero',
+  templateUrl: './footer-hero.component.html',
+  styleUrls: ['./footer-hero.component.css']
+})
+export class FooterHeroComponent {
+
+}
